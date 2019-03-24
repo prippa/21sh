4d8be5893a212f82a67c293e6e6d34277a6fa262
prippa <pavelrippa@gmail.com>
2019-03-24 22:12:08 +0200

    made ctrl+k and ctrl+u

--- source/read_line/key_events/ctrl/rl_ke_ctrl_k.c
+++ source/read_line/key_events/ctrl/rl_ke_ctrl_k.c
@@ -2,6 +2,10 @@
 
 int32_t		rl_ke_ctrl_k(t_line *ln)
 {
-	ft_printf("rl_ke_ctrl_k\n");
-	return (OK);
+	if (ln->l_cur_pos < ln->l_end)
+	{
+		rl_del_from_line(ln, ln->l_end, rl()->w_size.ws_col, true);
+		return (OK);
+	}
+	return (ERR);
 }
--- source/read_line/key_events/ctrl/rl_ke_ctrl_u.c
+++ source/read_line/key_events/ctrl/rl_ke_ctrl_u.c
@@ -2,6 +2,14 @@
 
 int32_t		rl_ke_ctrl_u(t_line *ln)
 {
-	ft_printf("rl_ke_ctrl_u\n");
-	return (OK);
+	size_t end;
+
+	if (ln->l_cur_pos > ln->l_start)
+	{
+		end = ln->l_cur_pos;
+		rl_ke_home(ln);
+		rl_del_from_line(ln, end, rl()->w_size.ws_col, true);
+		return (OK);
+	}
+	return (ERR);
 }
